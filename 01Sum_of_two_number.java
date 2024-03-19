32f90ed593689be19b14727161a0bf63897af446
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-19 19:39:57 +0530

    Sum of Two Number

---
+++ 01Sum_of_two_number.java
@@ -0,0 +1,16 @@
+class Sum_of_two_number{
+
+	public static void main(String [] args){
+
+		int a=1954,b=28413,sum;
+		sum=a+b;
+		System.out.println("Sum of Two Digit is "+sum);
+	}
+}
+
+/*
+OUTPUT::
+	Sum of Two Digit is 30367
+
+
+*/
