350cacc2ce5f5cb76cb92d7aa35a90846205813c
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-20 15:17:25 +0530

    Find Average of 5 Number

---
+++ 02Avg_Of_5_No.java
@@ -0,0 +1,15 @@
+class Avg_Of_5_NO{
+	public static void main(String [] args){
+		int n1=41,n2=10,n3=5,n4=17,n5=7;
+		int avg;
+		avg=(n1+n2+n3+n4+n5)/5;
+		System.out.println("Averge of 5 number is a: " +avg);
+	}
+};
+
+/*
+
+OUTPUT::
+	Averge of 5 number is a: 16
+
+*/
