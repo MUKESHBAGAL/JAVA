64158acc21a8b16887623eae00fd7f5b2366cc6e
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-21 21:22:16 +0530

    Swap Two Number

---
+++ 03Swap_Two__Number.java
@@ -0,0 +1,10 @@
+class Swap_Two_No{
+	public static void main(String[] args){
+	int a=10,b=20,c;
+	System.out.println("Before Swapping a= "+a+" b= " +b);
+	c=a;
+	a=b;
+	b=c;
+	System.out.println("After Swapping a= "+a+" b= " +b);
+	}
+}
