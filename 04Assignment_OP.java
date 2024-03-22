32cda6666c6b7bfdc421bedb4b68969ee457cc81
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-22 18:45:12 +0530

    Assignment Operator code

---
+++ 04Assignment_OP.java
@@ -0,0 +1,21 @@
+class Assignment_OP{
+	public static void main(String [] args){
+	int num1=10,num2=20;
+	
+	
+	num1+=num2;//here perform operation num1=num1+num2
+	System.out.println("The sum Assignment operator:" +num1);
+	
+	num1-=num2;//here perform operation num1=num1-num2
+	System.out.println("The Subtract Assignment operator:"+num1);
+	
+	num1*=num2;//here perform operation num1=num1*num2
+	System.out.println("The Multiply Assignment operator:"+num1);
+	
+	num1/=num2;//here perform operation num1=num1/num2
+	System.out.println("The Division Assignment operator:"+num1);
+	
+	num1%=num2;//here perform operation num1=num1%num2
+	System.out.println("The Modulus Assignment operator:"+num1);
+	}
+}
