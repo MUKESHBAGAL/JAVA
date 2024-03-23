71d2e3c7fa276d7db702ab21e8740428db9927c3
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-23 20:03:26 +0530

    Arithmetic Operator Program

---
+++ 05Arithmetic_OP.java
@@ -0,0 +1,21 @@
+class Arithmetic_OP{
+	public static void main(String [] args){
+	int num,num1=30,num2=40;
+	
+	
+	num=num1+num2;//here perform operation num1=num1+num2
+	System.out.println("The sum Arthmetic operator:"+num);
+	
+	num=num1-num2;//here perform operation num1=num1-num2
+	System.out.println("The Subtract Arthmetic operator:"+num);
+	
+	num=num1*num2;//here perform operation num1=num1*num2
+	System.out.println("The Multiply Arthmetic operator:"+num);
+	
+	num=num1/num2;//here perform operation num1=num1/num2
+	System.out.println("The Division Arthmetic operator:"+num);
+	
+	num=num1%num2;//here perform operation num1=num1%num2
+	System.out.println("The Modulus Arthmetic operator:"+num);
+	}
+}
