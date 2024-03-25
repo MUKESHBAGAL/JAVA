3dc63669c2d57d3498c8cad6a5e51018e57cce07
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-25 11:07:08 +0530

    Increment And Decrement Operator

---
+++ 07Increment_Decrement_OP.java
@@ -0,0 +1,10 @@
+class Increment_Decrement_OP{
+	public static void main(String [] args){
+	int x=10;
+	int y=12;
+	x++;
+	y--;
+	System.out.println("After Increment the number(Used Increment Operator): "+x);
+	System.out.println("After Decrement the number(Used Decrment Operator): "+y);
+	}
+}
