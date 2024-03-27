9994a98ca5a58cdd9d8d191f9e0fc9608e924691
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-27 09:17:30 +0530

    Write A Program of Even Odd Number

---
+++ 09Even_Odd.java
@@ -0,0 +1,11 @@
+class Even_Odd{
+	public static void main(String[] args){
+		int a=39;
+		if(a%2==0){
+			System.out.println("Number is a Even Number");
+		}
+		else{
+			System.out.println("Number is a Odd Number");
+		}
+	}
+}
