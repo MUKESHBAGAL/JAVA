9602dda48655479b7ddfb530acd4f0e7f7796716
mukeshBagal <2022bit039@sggs.ac.in>
2024-03-28 10:46:44 +0530

    Find the gretest Number from three Number

---
+++ 10Gretest_Number.java
@@ -0,0 +1,14 @@
+class Gretest_Number{
+	public static void main(String [] args){
+		int a=12,b=25,c=27;
+		if(a>b && a>c){
+		 System.out.println("a Is A Gretest Number ");
+		}
+		else if(b>a && b>c){
+			System.out.println("b Is A Gretest Number ");
+		}
+		else{
+			System.out.println("c Is A Gretest Number ");
+		}
+	}
+}
