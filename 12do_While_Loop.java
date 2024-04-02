2219811780bb6d5aaa0b0060e5e1e9f6a2323a47
mukeshBagal <2022bit039@sggs.ac.in>
2024-04-02 17:53:15 +0530

    Program Print 1 to 10 using do while loop

---
+++ 12do_While_Loop.java
@@ -0,0 +1,12 @@
+//This Program Print 1 to 10
+class do_While{
+	public static void main(String [] args){
+		int i=1;
+		do{
+		System.out.print(" "+i);
+		i++;
+		}while(i<=10);
+	
+	System.out.print(" \n");	
+	}
+}
