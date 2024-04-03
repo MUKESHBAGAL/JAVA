fdabd0b4815f7477297f74ab335ab2fab08d378f
mukeshBagal <2022bit039@sggs.ac.in>
2024-04-03 12:21:12 +0530

    Write a Program For Fibonacci Series

---
+++ 13For_Loop.java
@@ -0,0 +1,10 @@
+//Write a Program for a fibancci Series(means 1*2*3............9*10)
+class For_Loop{
+	public static void main(String [] args){
+		int i,fiba=1;
+		for (i=1;i<=10;i++){
+	           fiba=fiba*i;
+		}
+		System.out.println( "fibancci Series is a( 1*2*3............9*10) :"+fiba);
+	}
+}
