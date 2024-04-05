329b2aad9896ae1efba03b1c3ca6409eec7183a3
mukeshBagal <2022bit039@sggs.ac.in>
2024-04-06 03:42:23 +0530

    Write A program for Print Array Element

---
+++ 15Print_array_Element.java
@@ -0,0 +1,11 @@
+class Print_array{
+	public static void main(String[] args){
+		int array[]={1,2,3,4,5,6,7,8,9,10};
+		System.out.print("Array is :");
+               		for(int i=0;i<10;i++){
+                	System.out.print(+array[i]+" ");
+                
+             		}
+             	System.out.println("\n");
+	}
+}
