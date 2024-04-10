eb21289b7b6731514c3c696ff50603004766f854
mukeshBagal <2022bit039@sggs.ac.in>
2024-04-10 19:48:17 +0530

    Print Even Number in given array

---
+++ 16Print_Only_Even_Number_array.java
@@ -0,0 +1,12 @@
+class Print_Even{
+	public static void main(String[] args){
+		int array[]={1,2,3,4,5,6,7,8,9,10};
+		System.out.print("Array is :");
+               		for(int i=0;i<10;i++){
+               		if(array[i]%2==0){
+                		System.out.print(+array[i]+" ");
+                	}
+             		}
+             	System.out.println("\n");
+	}
+}
